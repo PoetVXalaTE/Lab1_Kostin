9b83ed54b7d6fa3a93f9499c8b5e9ad2711b747f
Егоров Илья <severozapad.178@gmail.com>
2026-04-08 23:52:03 +0300

    Created class Note

---
+++ Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public Note(string title, string content)
+        {
+            Title = title;
+            Content = content;
+            Date = DateTime.Now;
+        }
+    }
+}
